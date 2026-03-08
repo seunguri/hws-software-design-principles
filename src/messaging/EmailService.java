b3b63e343a17d6ad4808bbcd379b1215013a50f1
mendel <rhthrhrl0@naver.com>
2026-03-08 21:12:07 +0900

    refactor: MessageSender 인터페이스의 Email 및 KaKao send 메서드 구현

--- src/messaging/EmailService.java
+++ src/messaging/EmailService.java
@@ -1,4 +1,8 @@
 package messaging;
 
 // 2. 메일 발송 전문가
-public class EmailService implements MessageSender { public void send(String message) { /* 이메일 발송 */ }}
+public class EmailService implements MessageSender {
+    public void send(String message) { /* 이메일 발송 */
+        System.out.println("이메일 발송: " + message);
+    }
+}
--- src/messaging/KaKaoService.java
+++ src/messaging/KaKaoService.java
@@ -1,3 +1,12 @@
 package messaging;
 
-public class KaKaoService implements MessageSender { public void send(String message) { /* 카톡 발송*/ } }
+public class KaKaoService implements MessageSender {
+    public void send(String message) { /* 카톡 발송*/
+        // 카카오는 메일과 달리 글자수 제한이 있다고 가정
+        if (message.length() > 100) {
+            // 부모의 약속(보낸다)을 어기고 예외를 던짐
+            throw new RuntimeException("카톡은 100자 이상 보낼 수 없습니다!");
+        }
+        System.out.println("카톡 발송: " + message);
+    }
+}
